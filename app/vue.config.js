5891a6e0422d33f11f99c84670cb6830ea0e81ef
Max Mansfield <max.mansfield@zoom.us>
2022-04-05 04:56:41 -0600

    refactored project

--- app/vue.config.js
+++ app/vue.config.js
@@ -3,5 +3,4 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
     transpileDependencies: true,
-    outputDir: path.resolve(__dirname, '../dist/public'),
 });
