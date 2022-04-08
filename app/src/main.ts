6aa232432abdbff0f30cd3cceda3a54f56b66528
Max Mansfield <max.mansfield@zoom.us>
2022-04-08 00:56:01 -0600

    added deps

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,4 +3,11 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+import PrimeVue from 'primevue/config';
+
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+const app = createApp(App);
+app.use(store).use(router).use(PrimeVue).mount('#app');
